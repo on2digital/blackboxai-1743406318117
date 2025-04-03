a279b0da3fd293cfce3e81a984ecbcc6ae03c0fe
on2digital <on2digitalbd@gmail.com>
2025-04-03 14:25:44 +0600

    Add document-parsing-platform/frontend/src/App.jsx 1743668630918

--- document-parsing-platform/frontend/src/App.jsx
+++ document-parsing-platform/frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
+import UploadAndOutputPage from './pages/UploadAndOutputPage'
 import ConfigPage from './pages/ConfigPage'
-import OutputPage from './pages/OutputPage'
 import Navbar from './components/Navbar'
 
 export default function App() {
@@ -11,9 +10,8 @@ export default function App() {
         <Navbar />
         <main className="flex-1 p-6">
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<UploadAndOutputPage />} />
             <Route path="/configure" element={<ConfigPage />} />
-            <Route path="/output" element={<OutputPage />} />
           </Routes>
         </main>
       </div>
